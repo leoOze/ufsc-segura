28838c61108f5d15c07827c66bdeb44ece78b0e9
Matiko <matikorikotaka@gmail.com>
2026-05-21 17:37:34 -0300

    headersAtualizados

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -3,9 +3,9 @@ import { Tabs } from 'expo-router';
 export default function TabsLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="home" options={{ title: 'Home' }} />
-      <Tabs.Screen name="mainmap" options={{ title: 'Mapa' }} />
-      <Tabs.Screen name="estatisticas" options={{ title: 'Estatisticas' }} />
+      <Tabs.Screen name="home" options={{ title: 'Home' ,headerShown : false }} />
+      <Tabs.Screen name="mainmap" options={{ title: 'Mapa' ,headerShown : false }} />
+      <Tabs.Screen name="estatisticas" options={{ title: 'Estatisticas' ,headerShown : false }} />
     </Tabs>
   );
 }
